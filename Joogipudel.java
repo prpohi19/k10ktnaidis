856aa04e03e2ac3434c4997b2d48655822e03ad2
darikre <43129527+darikre@users.noreply.github.com>
2019-04-27 17:29:09 +0300

    1osa

---
+++ Joogipudel.java
@@ -0,0 +1,31 @@
+public class Joogipudel{
+    String nimetus;
+    enum pudelityyp{
+        klaas, plast, plekk
+    }
+    double maht;
+    double mass;
+    double hind;
+    Jook jook;
+    pudelityyp tyyp;
+    double taara_hind;
+
+    public Joogipudel(Jook jook, double maht, pudelityyp tyyp, double mass, double taara_hind){
+        this.jook = jook;
+        this.maht = maht;
+        this.tyyp = tyyp;
+        this.mass = mass;
+        this.taara_hind = taara_hind;
+    }
+
+    public double getMass(){
+        mass += maht*jook.erikaal;
+        return mass;
+    }
+
+    public double getPrice(){
+        
+		hind = (taara_hind/100)+maht/1000*jook.hind/100;
+        return hind;
+    }
+} 
