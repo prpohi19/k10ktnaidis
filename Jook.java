5395ef5522e249dbb8d5d9adc908864e69f53688
kuntervert <43129473+kuntervert@users.noreply.github.com>
2019-06-18 12:43:19 +0300

    Create Jook.java

---
+++ Jook.java
@@ -0,0 +1,11 @@
+public class Jook{
+  String nimi;
+  double hind;
+  double kaal;
+
+  public Jook(String nimi, double hind, double kaal){
+    this.nimi=nimi;
+    this.hind=hind;
+    this.kaal=kaal;
+  }
+}
